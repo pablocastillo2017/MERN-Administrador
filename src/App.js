2029a81106c81fb867d9208b3e8c81edceda12c6
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-05-14 14:26:48 -0400

    Proyecto 15 MERN 188. Creando los componentes y routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import NuevaCuenta from "./components/auth/NuevaCuenta";
+import Proyectos from "./components/proyectos/Proyectos";
 
+// Switch: todo lo que se coloque dentro del Switch
+// van a ser cada una de la diferentes paginas
+
+// y lo que este por fuera sera lo que se vea en todas la paginas
 function App() {
-  return <h1>MERN TASK</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;

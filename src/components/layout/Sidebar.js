67ac33f57bcde6e3294bdbb06c89780b93a4dbff
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-05-15 16:25:14 -0400

    MERN 17 -Primeros Pasos

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Sidebar = () => {
+  return (
+    <aside>
+      <h1>
+        MERN<span>Tasks</span>
+        <div className="proyectos">
+          <h2>Tus Proyectos</h2>
+        </div>
+      </h1>
+    </aside>
+  );
+};
+
+export default Sidebar;

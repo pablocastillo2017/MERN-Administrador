5dc27e2a74e901f8f143b4b5275c630a39221397
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-05-15 18:43:56 -0400

    MERN 17 - Creando formulario para nuevos Proyectos

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,13 +1,18 @@
 import React from "react";
+import NuevoProyecto from "../proyectos/NuevoProyecto";
+
 const Sidebar = () => {
   return (
     <aside>
       <h1>
         MERN<span>Tasks</span>
-        <div className="proyectos">
-          <h2>Tus Proyectos</h2>
-        </div>
       </h1>
+
+      <NuevoProyecto />
+
+      <div className="proyectos">
+        <h2>Tus Proyectos</h2>
+      </div>
     </aside>
   );
 };

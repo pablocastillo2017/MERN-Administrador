6b1fdf5f7b9d3563ffc08694719f77524f81e71a
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-05-19 13:12:13 -0400

    Sec 17 MERN -State y funciones para nuevos proyectos

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,18 +1,48 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const NuevoProyecto = () => {
+  // State Para Pryecto
+
+  const [proyecto, guardarProyecto] = useState({
+    nombre: "",
+  });
+
+  // Extraer nombre del proyecto
+  const { nombre } = proyecto;
+
+  // Lee los contenidos del INPUT
+  const onChangeProyecto = (e) => {
+    guardarProyecto({
+      ...proyecto,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Cuando el Usuario Envia un Proyecto
+  const onSubmitProyecto = (e) => {
+    e.preventDefault();
+
+    // Validar el Proyecto
+
+    // Agregar al State
+
+    // Reniniciar el FORM
+  };
+
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         Nuevo Proyecto
       </button>
 
-      <form className="formulario-nuevo-proyecto">
+      <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
         <input
           type="text"
           className="input-text"
           placeholder="Nombre Proyecto"
           name="nombre"
+          value={nombre}
+          onChange={onChangeProyecto}
         />
 
         <input

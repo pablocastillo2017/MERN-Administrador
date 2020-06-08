c34508f378055240c3fd0a1562fbf6d169b128a1
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-06-07 23:13:24 -0400

    MERN 18 - Mostrando el Nombre del Proyecto

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,7 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
+import proyectoContext from "../context/proyectos/proyectoContext";
+
 import Tarea from "./Tarea";
 
 const ListadoTareas = () => {
+  // Obener Proyecto de;   state Inicial
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  // si no hay proyecto seleccionado
+  if (!proyecto) return <h2>No hay Proyecto Seleccionado</h2>;
+
+  // Array destructuring para estraer proyecto Actual
+  const [proyectoActual] = proyecto;
+
   const tareasProyecto = [
     { nombre: "Elegir Plataforma", estado: true },
     { nombre: "Elegir Colores", estado: false },
@@ -11,7 +23,7 @@ const ListadoTareas = () => {
 
   return (
     <Fragment>
-      <h2>Proyecto: Tienda Virtual</h2>
+      <h2>Proyecto: {proyectoActual.nombre}</h2>
       <ul className="listado-tareas">
         {tareasProyecto.length === 0 ? (
           <li className="tarea">

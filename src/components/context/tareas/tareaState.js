a1e08430e10bddd245a9c3aee18bfffc4aa167d8
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-06-19 18:27:10 -0400

    MERN 19 - creando funcion para que obtenga las tareas del proyecto

--- src/components/context/tareas/tareaState.js
+++ src/components/context/tareas/tareaState.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
 import TareaContext from "./tareaContex";
 import TareaReducer from "./tareaReducer";
+import { TAREAS_PROYECTO } from "../../../types";
 
 const TareaState = (props) => {
   const initialState = {
@@ -24,10 +25,21 @@ const TareaState = (props) => {
   // Crear Dispach y State
   const [state, dispach] = useReducer(TareaReducer, initialState);
 
+  // Crear las Funciones
+
+  // Obtener las tareas de un proyecto
+  const obtenerTareas = (proyectoId) => {
+    dispach({
+      type: TAREAS_PROYECTO,
+      paylod: proyectoId,
+    });
+  };
+
   return (
     <TareaContext.Provider
       value={{
         tareas: state.tareas,
+        obtenerTareas,
       }}
     >
       {props.children}
--- src/components/proyectos/Proyecto.js
+++ src/components/proyectos/Proyecto.js
@@ -1,19 +1,30 @@
 import React, { useContext } from "react";
 import proyectoContext from "../context/proyectos/proyectoContext";
+import tareaContext from "../context/tareas/tareaContex";
 
 const Proyecto = ({ proyecto }) => {
   // Obener el state de Proyectos
   const proyectosContext = useContext(proyectoContext);
-
   // Extraer formulario de proyectoContext.Provider
   const { proyectoActual } = proyectosContext;
+
+  // Obtener la Funcion del contex de tarea
+  const tareasContext = useContext(tareaContext);
+  // Extraer la funcion de tareaContext
+  const { obtenerTareas } = tareasContext;
+
+  // funcion para agregar el EVENTO actual
+  const seleccionarProyecto = (id) => {
+    proyectoActual(id); // fijar un proyecto actual
+    obtenerTareas(id); // filtrar las tareas por id cuando se de click
+  };
 
   return (
     <li>
       <button
         type="button"
         className="btn btn-blank"
-        onClick={() => proyectoActual(proyecto.id)}
+        onClick={() => seleccionarProyecto(proyecto.id)}
       >
         {proyecto.nombre}
       </button>
